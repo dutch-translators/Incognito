77ae6fe00f92871b9321d802cc5dcdc21dced922
David <neurofibromatose@live.nl>
2015-02-15 18:10:50 +0100

    eerste vertaling

--- nl/common.php
+++ nl/common.php
@@ -4,6 +4,7 @@
  * @package phpBB Extension - Incognito
  * @copyright (c) 2015 steveurkel
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
  *
  */
 
@@ -18,13 +19,13 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-		'ANONYM'						=> 'Post as anonymous',
-		'ANONYM_EXPLAIN'				=> 'Check this field if you want to post anonymous. This cannot be reverted once the post has been sent.',
+		'ANONYM'						=> 'Anoniem plaatsen',
+		'ANONYM_EXPLAIN'				=> 'Selecteer deze optie als je dit bericht anoniem wilt plaatsen. Dit kan niet worden teruggedraaid als het bericht al geplaatst is.',
 		
 		'ACP_INCOGNITO_TITLE'			=> 'Incognito Module',
-		'ACP_INCOGNITO'					=> 'Settings',
-		'ACP_INCOGNITO_GOODBYE'			=> 'Post as anonymous active?',
-		'ACP_INCOGNITO_SETTING_SAVED'	=> 'Settings have been saved successfully!',
-		'ACP_INCOGNITO_NAME'			=> 'Displayed username',
-		'ACP_INCOGNITO_NAME_EXPLAIN'	=> 'Entered value will be shown as username for post. When left empty the standard value will be shown.',
+		'ACP_INCOGNITO'					=> 'Instellingen',
+		'ACP_INCOGNITO_GOODBYE'			=> 'Activeer Anoniem plaatsen?',
+		'ACP_INCOGNITO_SETTING_SAVED'	=> 'Instellingen zijn succesvol opgeslagen!',
+		'ACP_INCOGNITO_NAME'			=> 'Gebruikersnaam weergeven',
+		'ACP_INCOGNITO_NAME_EXPLAIN'	=> 'Vul de gebruikersnaam in die je bij anonieme berichten wilt gebruiken. Als je deze optie leeg laat wordt de standaard waarde gebruikt.',
 ));
